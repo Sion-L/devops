db9c3ce4706b99cee222bbf824d9ce8d19c3ebb4
Lang <lilang@firecloud.ai>
2024-09-13 17:44:14 +0800

    代码调整

--- core/user/jwt.go
+++ core/user/jwt.go
@@ -3,9 +3,6 @@ package user
 import "github.com/golang-jwt/jwt/v4"
 
 func GetJwtToken(secretKey string, iat, seconds, refreshAfter, userId int64, roleType int64) (string, error) {
-	// 角色映射 塞到token里面去
-	// jwtToken, err2 := core.GetJwtToken(l.svcCtx.Config.Auth.AccessSecret,
-	//		now, accessExpire, res.UserId, res.RoleType)
 	roleMap := map[int64]string{1: "admin", 2: "dev"}
 	claims := make(jwt.MapClaims)
 	claims["exp"] = iat + seconds
